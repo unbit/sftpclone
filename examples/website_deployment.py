14e5d70e8f281496eed00dbe4819d35679dfed97
Adriano Di Luzio <adrianodl@hotmail.it>
2018-09-03 12:12:36 +0200

    Reformat examples

--- examples/website_deployment.py
+++ examples/website_deployment.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-# sftpclone can be imported into a custom Python script to deploy a
-# static website to a web host where you only have stripped-down sftp
-# access. The below is an example of a deployment script that keeps
-# your username and password out of the shell history and uses an
-# exclude file. In practice, one might have complex needs. For
-# example, the website may be built from assets in multiple,
-# separately managed directory trees on the local host. Using
-# sftpclone programmatically with exclude files makes it easy to
-# script one-step deployments for such situations.
+"""
+sftpclone can be imported into a custom Python script to deploy a static website
+to a web host where you only have stripped-down sftp access.
+
+The below is an example of a deployment script that keeps
+your username and password out of the shell history and uses an exclude file.
+
+In practice, one might have complex needs.
+For example, the website may be built from assets in multiple, separately managed directory trees on the local host.
+Using sftpclone programmatically with exclude files makes it easy to script one-step deployments for such situations.
+"""
 
 import getpass
-import os
+
 from sftpclone import sftpclone
 
-def get_username_and_password():
+
+def _get_username_and_password():
     username = input("Username [{}]: ".format(getpass.getuser()))
+
     if not username:
         username = getpass.getuser()
-
     password = getpass.getpass()
 
-    return (username, password)
+    return username, password
+
 
 def deploy_assets(username, password):
     # The local path and the remote path in this function should
     # correspond to where you build the static site locally and where
     # it should go on the remote sftp server.
-    cloner = sftpclone.SFTPClone(
+    sftpclone.SFTPClone(
         './build',
         "{}:{}@mysite.example.com:./mysite".format(username, password),
-        exclude_file = 'exclude.txt'
-    )
-    cloner.run()
+        exclude_file='exclude.txt'
+    ).run()
+
+
+def main():
+    username, password = _get_username_and_password()
+    deploy_assets(username, password)
+
 
 if __name__ == "__main__":
-    username, password = get_username_and_password()
-    deploy_assets(username, password)
+    main()
