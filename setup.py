90fbdd35f446285f3cf9641981f35cadf0218f1d
Adriano Di Luzio <adrianodl@hotmail.it>
2018-09-03 12:12:56 +0200

    Add Python 3.7 classifier

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Development Status :: 6 - Mature",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
